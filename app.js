3fc7a1ca5f1668715b852d6ccd80282e0514b4c5
caolam23 <caol69265@gmail.com>
2026-03-05 14:56:34 +0700

    Fix loi 404 va cap nhat API cho Role, User

--- app.js
+++ app.js
@@ -9,9 +9,9 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+// view engine setup - removed for API only
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -20,7 +20,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/api/v1/users', usersRouter);
 app.use('/api/v1/products', require('./routes/products'))
 app.use('/api/v1/categories', require('./routes/categories'))
 app.use('/api/v1/roles', require('./routes/roles'))
@@ -37,9 +37,13 @@ app.use(function (err, req, res, next) {
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
+  // send JSON error response
   res.status(err.status || 500);
-  res.render('error');
+  res.json({
+    message: err.message,
+    status: err.status || 500,
+    error: req.app.get('env') === 'development' ? err : {}
+  });
 });
 
 module.exports = app;
